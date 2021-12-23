398b8bd2a4063e018a4c630edcfa95c13c25ca82
Erin Clark <erincodepirate@gmail.com>
2021-12-22 17:19:25 -0800

    Add users struct to webservice

--- webservice/main.go
+++ webservice/main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/erincodepirate/go-test/webservice/models"
+)
 
 func main() {
-	fmt.Println("Hello from module")
+	u := models.User{
+		ID:        2,
+		FirstName: "Rosa",
+		LastName:  "Luxemburg",
+	}
+	fmt.Println(u)
 }
