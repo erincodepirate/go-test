a57290c50469f0e4ce6422d3848177476f8b56d0
Erin Clark <erincodepirate@gmail.com>
2021-12-22 17:39:29 -0800

    Add parameters, returns to function

--- function/main.go
+++ function/main.go
@@ -1,12 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	startWebServer()
+	port := 3000
+	_, err := startWebServer(port, 2)
+	fmt.Println(err)
 }
 
-func startWebServer() {
+func startWebServer(port, retries int) (int, error) {
 	fmt.Println("Starting server")
-	fmt.Println("Server started")
+	fmt.Println("Server started on port", port)
+	fmt.Println("Number of retries", retries)
+
+	//return errors.New("Something went wrong")
+	return port, nil
 }
