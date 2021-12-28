3e2680d5fc35a094b9a8adadfbc7909c310d22fd
Erin Clark <erincodepirate@gmail.com>
2021-12-27 17:00:59 -0800

    Add user functions to webservice

--- webservice/models/user.go
+++ webservice/models/user.go
@@ -1,5 +1,7 @@
 package models
 
+import "fmt"
+
 type User struct {
 	ID        int
 	FirstName string
@@ -21,4 +23,34 @@ func AddUser(u User) (User, error) {
 	users = append(users, &u)
 
 	return u, nil
+}
+
+func GetUserByID(id int) (User, error) {
+	for _, u := range users {
+		if u.ID == id {
+			return *u, nil
+		}
+	}
+
+	return User{}, fmt.Errorf("User with ID '%v' not found.", id)
+}
+
+func UpdateUser(u User) (User, error) {
+	for i, candidate := range users {
+		if candidate.ID == u.ID {
+			users[i] = &u
+			return u, nil
+		}
+	}
+	return User{}, fmt.Errorf("User with ID '%v' not found.", u.ID)
+}
+
+func RemoveUserById(id int) error {
+	for i, u := range users {
+		if u.ID == id {
+			users = append(users[:i], users[i+1:]...)
+			return nil
+		}
+	}
+	return fmt.Errorf("User with ID '%v' not found.", id)
 }
